e1254786618746cd8d05156b28efb67d8e3e2c29
luigi <antoinelisberto@gmail.com>
2020-07-17 17:08:29 +0800

    possible success

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import { Switch, Route } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//         <Route exact path="/rooms/" component={Rooms} />
+//         <Route exact path="/rooms/:slug" component={SingleRoom} />
+//         <Route component={Error} />
+//       </Switch>
+//     </>
+//   );
+// }
+
+// export default App;
